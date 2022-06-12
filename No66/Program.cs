4521bcec7f705c6552ddacbe3a16b1783c113e12
Katherine_20 <mob1911494@gmail.com>
2022-06-12 16:49:34 +0300

    Сумма чисел между двумя заданными

---
+++ No66/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int GetNumber (string msg)
+{
+    Console.WriteLine(msg);
+    string value_from_console = Console.ReadLine();
+    if(int.TryParse(value_from_console, out int num))
+    {
+        if (num > 0) return num;
+        else 
+        {
+            Console.WriteLine("Введите положительное число.");
+        }
+    }
+    else 
+    {
+        Console.WriteLine("Введите число.");
+    }
+    return num;
+}
+
+int SummaChisel (int M, int N)
+{
+    if (M != N)
+    {
+       M = SummaChisel(M,N-1) + N;
+    }
+    return M;
+}
+
+string messageN = "Введите N: ";
+string messageM = "Введите M: ";
+int N = GetNumber(messageN);
+int M = GetNumber(messageM);
+int res = SummaChisel(M,N);
+Console.WriteLine($"Сумма элементов между ними равна {res}");
